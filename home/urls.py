a47d19eae23ae0e54bd3eb235b914f0674d721f9
Uzytkownik <uzytkownik@kursGIT.pl>
2022-06-05 09:25:06 +0200

    project update - v. 2.1

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,12 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('offers/', views.OffersList.as_view(), name='offers'),
+    path('islandia/', views.islandia, name='islandia'),
+    path('norwegia/', views.norwegia, name='norwegia'),
+    path('szwecja/', views.szwecja, name='szwecja'),
+    path('dania/', views.dania, name='dania'),
+    path('blog/', views.blog, name='blog'),
+    path('oferta_specjalna/', views.oferta_specjalna, name='oferta_specjalna'),
+    path('last_minute/', views.oferta_specjalna, name='last-minute'),
+    path('about_us/', views.onas, name='onas'),
 ]
--- home/views.py
+++ home/views.py
@@ -7,6 +7,26 @@ from home.models import Offer
 def index(request):
     return render(request, 'home.html')
 
+def islandia(request):
+    return render(request, 'islandia.html')
+
+def dania(request):
+    return render(request, 'dania.html')
+
+def szwecja(request):
+    return render(request, 'szwecja.html')
+
+def norwegia(request):
+    return render(request, 'norwegia.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def oferta_specjalna(request):
+    return render(request, 'oferta_specjalna.html')
+
+def onas(request):
+    return render(request, 'onas.html')
 
 class OffersList(ListView):
     model = Offer
